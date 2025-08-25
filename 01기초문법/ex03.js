3e2b9d6a993536a527d685fea64be7a8cc18da21
EatingIting <euijae0611@naver.com>
2025-08-25 17:10:05 +0900

    JS 예제파일

---
+++ 01기초문법/ex03.js
@@ -0,0 +1,19 @@
+//대소문자를 구분한다.
+var name = "홍길동";
+var Name = "이순신";
+
+console.log(name,Name);
+
+//붙어있는 단어의 첫글자는 대문자로 사용한다.
+//이름에 띄어쓰기는 들어갈 수 없다.
+
+var phoneNumber = 1234; /* var phone_number != //연결되는 단어의 첫번째는 대문자로 표기한다. */
+console.log(phoneNumber);
+
+//키워드는 이름으로 쓸 수 없다.
+
+/* var var = 1; //안됨
+var for = 1; //안됨
+var class = 1; //안됨
+var if = 1; //안됨 */
+
