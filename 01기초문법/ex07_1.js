228e5a1f24eb9f741ee66c70cd39648f3653032c
EatingIting <euijae0611@naver.com>
2025-08-26 11:07:53 +0900

    수정없음

--- 01기초문법/ex07_1.js
+++ 01기초문법/ex07_1.js
@@ -5,3 +5,17 @@ let b = 5 / 3;
 let c = 5 % 3;
 
 console.log(a, b, c);
+console.log(parseInt(b)); //실수 -> 정수로 변경
+
+//전위연산 후위연산 차이
+let x = 5;
+let pre = ++x;
+console.log("x는: ",x);
+console.log("pre는: ",pre);
+
+//후위연산 - 먼저 대입하고, 이후에 증가
+let y = 5;
+let post = y++;
+console.log("y는: ",y);
+console.log("post는: ",post);
+
